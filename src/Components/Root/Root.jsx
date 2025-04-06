9e873f1cb57aa061866d92e69ae27395ddb6a690
H-M-Nizum <hmnizum1714032@gmail.com>
2025-04-07 01:35:15 +0600

    [Setup]- Setup daisyUI and test it

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -6,6 +6,8 @@ const Root = () => {
       <h3 className="text-3xl font-bold underline text-red-700">
         This is Root For Test
       </h3>
+      <br />
+      <button className="btn btn-dash">Daisy Test Button</button>
     </div>
   );
 };
