499ec0693cd7eb9d93ac6a79c179c9d6d48a82ba
H-M-Nizum <hmnizum1714032@gmail.com>
2025-04-07 01:30:27 +0600

    [Setup] - Setup Tailwindcss and test it

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Root = () => {
   return (
     <div>
-      <h3>This is Root For Test</h3>
+      <h3 className="text-3xl font-bold underline text-red-700">
+        This is Root For Test
+      </h3>
     </div>
   );
 };
